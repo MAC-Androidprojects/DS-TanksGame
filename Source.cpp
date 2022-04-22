55d97c2b5785f3542ada538f486b0a97b3f23f19
Hesham Ashraf <heshamashraf573@gmail.com>
2022-04-22 06:19:34 +0200

    basic layout mod1

--- Source.cpp
+++ Source.cpp
@@ -17,14 +17,25 @@ using namespace std;
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 COORD cursor_position; // To specify the coordinates
 
-
-char g_tank[4][3]={
-	{'*','*','*'},
-	{' ','*',' '},
-	{' ','*',' '},
-	{'*','*','*'}
+/*Genral tank "for user and for the enemy"*/
+char g_tank[4][4]={
+	{254,' ',' ',254},
+	{254,254,254,254},
+	{254,254,254,254},
+	{254,' ',' ',254}
 };
 
+char g_my_rocket[2][4]={
+	{193,193,193,193},
+	{' ',193,193,' '}
+};
+
+char g_enemy_rocket[2][4]={
+	{194,194,194,194},
+	{' ',194,194,' '}
+};
+
+/*the x positions for user and enemy .. y position is constant for both "user and enemy"*/
 int g_my_tank_position_x = 0;
 int g_enemy_tank_position_x = 0;
 
@@ -53,17 +64,18 @@ void draw_bourder(){
 		/*this loop used to draw the bourders of the left side*/
 		for(int col = 0 ; col < 10 ; col++){
 			gotoxy(col , row);
-			cout<<"+";
+			cout<<char(178);
 		}
 		/*this loop used to draw the bourders of the rigth side*/
 		for(int col = SCREEN_WIDTH - 10 ; col < SCREEN_WIDTH; col++){
 			gotoxy(col , row);
-			cout<<"+";
+			cout<<char(178);
 		}
 	}
 
 }
 
+/*---------------- sector for user elements---------------------*/
 /*function used to draw the user's tank */
 void draw_my_tank(){
 	/*for loop to get the tank coordenates and draw it*/
@@ -71,13 +83,31 @@ void draw_my_tank(){
 	for(int posy = 0 ; posy < 4 ; posy++){
 		gotoxy((g_my_tank_center - 1) , (MY_TANK_POSTIONY - posy));
 		/*this loop to detect the x of the wigth of the tank*/
-		for(int posx = 0 ; posx < 3 ; posx++){
+		for(int posx = 0 ; posx < 4 ; posx++){
 			/*draw the tank using the array g_tank*/
 			cout<<g_tank[posy][posx];
 		}
 	}
 }
 
+/*function used to draw the user's rocket */
+void draw_my_rocket(){
+	/*for loop to get the racket coordenates and draw it*/
+	/*this loop to detect the y of the height of the rocket*/
+	for(int posy = 0 ; posy < 2 ; posy++){
+		gotoxy((g_my_tank_center - 1) , (MY_TANK_POSTIONY - 4 - posy));
+		/*this loop to detect the x of the wigth of the rocket*/
+		for(int posx = 0 ; posx < 4 ; posx++){
+			/*draw the user rocket using the array g_my_rocket*/
+			cout<<g_my_rocket[posy][posx];
+		}
+	}
+
+}
+/*--------------------------------------------------------------------------------*/
+
+
+/*---------------- sector for enemy elements---------------------*/
 /*function used to draw the PC's tank */
 void draw_enemy_tank(){
 	/*for loop to get the tank coordenates and draw it*/
@@ -85,7 +115,7 @@ void draw_enemy_tank(){
 	for(int posy = 0 ; posy < 4 ; posy++){
 		gotoxy((g_enemy_tank_center - 1) , (posy));
 		/*this loop to detect the x of the wigth of the tank*/
-		for(int posx = 0 ; posx < 3 ; posx++){
+		for(int posx = 0 ; posx < 4 ; posx++){
 			/*draw the tank using the array g_tank*/
 			cout<<g_tank[posy][posx];
 		}
@@ -93,10 +123,29 @@ void draw_enemy_tank(){
 
 }
 
+/*function used to draw the enemy's rocket */
+void draw_enemy_rocket(){
+	/*for loop to get the racket coordenates and draw it*/
+	/*this loop to detect the y of the height of the rocket*/
+	for(int posy = 0 ; posy < 2 ; posy++){
+		gotoxy((g_enemy_tank_center - 1) , (posy + 4 ));
+		/*this loop to detect the x of the wigth of the rocket*/
+		for(int posx = 0 ; posx < 4 ; posx++){
+			/*draw the racket using the array g_enemy_rocket*/
+			cout<<g_enemy_rocket[posy][posx];
+		}
+	}
+
+}
+/*--------------------------------------------------------------------------------*/
+
 /****************************************************/
 int main(){
+	/*calling just for test*/
 	draw_enemy_tank();
+	draw_enemy_rocket();
 	draw_my_tank();
+	draw_my_rocket();
 	draw_bourder();
 
 	cout<<endl;
