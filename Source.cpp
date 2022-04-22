3ee22804d057a4b6aab57bdbf4778bb098957550
Mohamed Emad <bon.dok2000@hotmail.com>
2022-04-22 13:06:28 +0200

    Changed values to integers

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <windows.h>
-#include<conio.h>
+#include <conio.h>
 #include <time.h>
+#include <string>
 
 using namespace std;
 
@@ -18,33 +19,29 @@ HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 COORD cursor_position; // To specify the coordinates
 
 /*Genral tank "for user and for the enemy"*/
-char g_tank[4][4]={
-	{254,' ',' ',254},
-	{254,254,254,254},
-	{254,254,254,254},
-	{254,' ',' ',254}
-};
+int g_tank[4][4] = {
+	{254, 32, 32, 254},
+	{254, 254, 254, 254},
+	{254, 254, 254, 254},
+	{254, 32, 32, 254}};
 
-char g_my_rocket[2][4]={
-	{193,193,193,193},
-	{' ',193,193,' '}
-};
+int g_my_rocket[2][4] = {
+	{193, 193, 193, 193},
+	{32, 193, 193, 32}};
 
-char g_enemy_rocket[2][4]={
-	{194,194,194,194},
-	{' ',194,194,' '}
-};
+int g_enemy_rocket[2][4] = {
+	{194, 194, 194, 194},
+	{32, 194, 194, 32}};
 
 /*the x positions for user and enemy .. y position is constant for both "user and enemy"*/
 int g_my_tank_position_x = 0;
 int g_enemy_tank_position_x = 0;
 
 /*to calculate the center position of the tanck*/
-int g_my_tank_center = SCREEN_WIDTH/2;
+int g_my_tank_center = SCREEN_WIDTH / 2;
 
 /*to calculate the center position of the tanck*/
-int g_enemy_tank_center = SCREEN_WIDTH/2;
-
+int g_enemy_tank_center = SCREEN_WIDTH / 2;
 
 /****************************************************/
 
@@ -52,95 +49,109 @@ int g_enemy_tank_center = SCREEN_WIDTH/2;
 void gotoxy(int x, int y)
 {
 	/*to determine the x position of the cursor in the console */
-    cursor_position.X = x; 
+	cursor_position.X = x;
 	/*to determine the y position of the cursor in the console */
-    cursor_position.Y = y;
+	cursor_position.Y = y;
 	/*passing the x,y position to the fucnction SetConsoleCursorPosition to move the cursor to the determined position*/
-    SetConsoleCursorPosition(console, cursor_position);
+	SetConsoleCursorPosition(console, cursor_position);
 }
 
-void draw_bourder(){
-	for(int row = 0 ; row < SCREEN_HEIGHT; row++){
+void draw_bourder()
+{
+	for (int row = 0; row < SCREEN_HEIGHT; row++)
+	{
 		/*this loop used to draw the bourders of the left side*/
-		for(int col = 0 ; col < 10 ; col++){
-			gotoxy(col , row);
-			cout<<char(178);
+		for (int col = 0; col < 10; col++)
+		{
+			gotoxy(col, row);
+			cout << char(178);
 		}
 		/*this loop used to draw the bourders of the rigth side*/
-		for(int col = SCREEN_WIDTH - 10 ; col < SCREEN_WIDTH; col++){
-			gotoxy(col , row);
-			cout<<char(178);
+		for (int col = SCREEN_WIDTH - 10; col < SCREEN_WIDTH; col++)
+		{
+			gotoxy(col, row);
+			cout << char(178);
 		}
 	}
-
 }
 
 /*---------------- sector for user elements---------------------*/
 /*function used to draw the user's tank */
-void draw_my_tank(){
+void draw_my_tank()
+{
 	/*for loop to get the tank coordenates and draw it*/
 	/*this loop to detect the y of the height of the tank*/
-	for(int posy = 0 ; posy < 4 ; posy++){
-		gotoxy((g_my_tank_center - 1) , (MY_TANK_POSTIONY - posy));
+	for (int posy = 0; posy < 4; posy++)
+	{
+		gotoxy((g_my_tank_center - 1), (MY_TANK_POSTIONY - posy));
 		/*this loop to detect the x of the wigth of the tank*/
-		for(int posx = 0 ; posx < 4 ; posx++){
+		for (int posx = 0; posx < 4; posx++)
+		{
 			/*draw the tank using the array g_tank*/
-			cout<<g_tank[posy][posx];
+			cout << char(g_tank[posy][posx]);
 		}
 	}
 }
 
 /*function used to draw the user's rocket */
-void draw_my_rocket(){
+void draw_my_rocket()
+{
 	/*for loop to get the racket coordenates and draw it*/
 	/*this loop to detect the y of the height of the rocket*/
-	for(int posy = 0 ; posy < 2 ; posy++){
-		gotoxy((g_my_tank_center - 1) , (MY_TANK_POSTIONY - 4 - posy));
+	for (int posy = 0; posy < 2; posy++)
+	{
+		gotoxy((g_my_tank_center - 1), (MY_TANK_POSTIONY - 4 - posy));
 		/*this loop to detect the x of the wigth of the rocket*/
-		for(int posx = 0 ; posx < 4 ; posx++){
+		for (int posx = 0; posx < 4; posx++)
+		{
 			/*draw the user rocket using the array g_my_rocket*/
-			cout<<g_my_rocket[posy][posx];
+			cout << char(g_my_rocket[posy][posx]);
 		}
 	}
-
 }
 /*--------------------------------------------------------------------------------*/
 
-
 /*---------------- sector for enemy elements---------------------*/
 /*function used to draw the PC's tank */
-void draw_enemy_tank(){
+void draw_enemy_tank()
+{
 	/*for loop to get the tank coordenates and draw it*/
 	/*this loop to detect the y of the height of the tank*/
-	for(int posy = 0 ; posy < 4 ; posy++){
-		gotoxy((g_enemy_tank_center - 1) , (posy));
+	for (int posy = 0; posy < 4; posy++)
+	{
+		gotoxy((g_enemy_tank_center - 1), (posy));
 		/*this loop to detect the x of the wigth of the tank*/
-		for(int posx = 0 ; posx < 4 ; posx++){
+		for (int posx = 0; posx < 4; posx++)
+		{
 			/*draw the tank using the array g_tank*/
-			cout<<g_tank[posy][posx];
+			cout << char(g_tank[posy][posx]);
 		}
 	}
-
 }
 
 /*function used to draw the enemy's rocket */
-void draw_enemy_rocket(){
+void draw_enemy_rocket()
+{
 	/*for loop to get the racket coordenates and draw it*/
 	/*this loop to detect the y of the height of the rocket*/
-	for(int posy = 0 ; posy < 2 ; posy++){
-		gotoxy((g_enemy_tank_center - 1) , (posy + 4 ));
+	for (int posy = 0; posy < 2; posy++)
+	{
+		gotoxy((g_enemy_tank_center - 1), (posy + 4));
 		/*this loop to detect the x of the wigth of the rocket*/
-		for(int posx = 0 ; posx < 4 ; posx++){
+		for (int posx = 0; posx < 4; posx++)
+		{
 			/*draw the racket using the array g_enemy_rocket*/
-			cout<<g_enemy_rocket[posy][posx];
+			cout << char(g_enemy_rocket[posy][posx]);
 		}
 	}
-
 }
 /*--------------------------------------------------------------------------------*/
 
 /****************************************************/
-int main(){
+int main()
+{
+	/* To clear the whole screen*/
+	system("cls");
 	/*calling just for test*/
 	draw_enemy_tank();
 	draw_enemy_rocket();
@@ -148,6 +159,6 @@ int main(){
 	draw_my_rocket();
 	draw_bourder();
 
-	cout<<endl;
+	cout << endl;
 	return 0;
 }
